dbccbca21415c9192eefb04a8da2b33aee3fe145
ben-laird <86321957+ben-laird@users.noreply.github.com>
2022-06-27 12:19:48 -0500

    Add timer component

---
+++ src/features/timer/Timer.tsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class Timer extends Component {
+  componentDidMount = () => {};
+
+  componentDidUpdate = () => {};
+
+  render() {
+    return <div>Timer</div>;
+  }
+}
+
+export default Timer;
