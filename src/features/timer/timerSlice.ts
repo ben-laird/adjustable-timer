3eec985547ec572769aca74c0fe36565a74d25fd
ben-laird <86321957+ben-laird@users.noreply.github.com>
2022-06-27 14:42:23 -0500

    Implement Dayjs duration plugin

--- src/features/timer/timerSlice.ts
+++ src/features/timer/timerSlice.ts
@@ -1,12 +1,19 @@
 import { createSlice, PayloadAction as Act } from "@reduxjs/toolkit";
-import dayjs, { ManipulateType } from "dayjs";
+import dayjs, { Dayjs, ManipulateType } from "dayjs";
+import duration from "dayjs/plugin/duration";
 
-type AddAction = Act<{ value: number; unit: ManipulateType }>;
+type DayAddAct = Act<{ value: number; unit: ManipulateType }>;
+type timerSlIS = typeof initialState;
+
+dayjs.extend(duration);
+
+const getDuration = (time1: Dayjs, time2: Dayjs) =>
+  dayjs.duration(time1.diff(time2));
 
 const initialState = {
   currentTime: dayjs(),
   targetTime: dayjs().add(5, "minute"),
-  tMinus: 5,
+  tMinus: dayjs.duration(5, "minutes"),
   isCounting: false,
 };
 
@@ -14,16 +21,21 @@ export default createSlice({
   name: "timer",
   initialState,
   reducers: {
-    advanceTime: (state, action: AddAction) => {
+    advanceTime: (state, action: DayAddAct) => {
       const { value, unit } = action.payload;
+      const { currentTime, targetTime, isCounting } = state;
+
       state.currentTime = dayjs();
-      if (!state.isCounting)
-        state.targetTime = state.targetTime.add(value, unit);
-      state.tMinus = state.currentTime.diff(state.targetTime, "seconds");
+      if (!isCounting) state.targetTime = targetTime.add(value, unit);
+      state.tMinus = getDuration(currentTime, targetTime);
     },
-    adjustTarget: (state, action: AddAction) => {
+    setTarget: (state, action: Act<Dayjs>) => {
+      const isValid = true;
+    },
+    adjustTarget: (state, action: DayAddAct) => {
       const { value, unit } = action.payload;
       state.targetTime = state.targetTime.add(value, unit);
+      state.tMinus = dayjs.duration(state.currentTime.diff(state.targetTime));
     },
     setCounting: (state, action: Act<boolean>) => {
       state.isCounting = action.payload;
