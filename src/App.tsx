5e146fc3faa9a33d7a6c80dd17b96c6c342d5672
ben-laird <86321957+ben-laird@users.noreply.github.com>
2022-06-28 09:50:20 -0500

    Develop Timer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import Timer from "./features/timer/Timer";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1>Time Stream</h1>
+      <h2>Timer for streamers</h2>
+      <Timer />
+    </div>
+  );
 };
 
 export default App;
--- src/features/timer/Timer.tsx
+++ src/features/timer/Timer.tsx
@@ -1,13 +1,32 @@
-import { Component } from "react";
+import { connect } from "react-redux";
+import { RootState } from "../../app/store";
+import { advanceTimeBy, flipCounting } from "./timerSlice";
 
-class Timer extends Component {
-  componentDidMount = () => {};
+type TimerProps = ReturnType<typeof mapState> & typeof mapDispatch;
 
-  componentDidUpdate = () => {};
+const Timer = (props: TimerProps) => {
+  const advanceUp = () => props.advanceTimeBy({ value: 1, unit: "second" });
 
-  render() {
-    return <div>Timer</div>;
-  }
-}
+  const dateString = "DD MMM, YYYY - h:mm:ss";
 
-export default Timer;
+  return (
+    <div>
+      <h3>{props.tMinus.format("HH:mm:ss")}</h3>
+      <p>
+        Counting from {props.currentTime.format(dateString)} to{" "}
+        {props.targetTime.format(dateString)}
+      </p>
+      <button onClick={advanceUp}>Advance time</button>
+      <button onClick={() => props.flipCounting()}>Start/stop counting</button>
+    </div>
+  );
+};
+
+const mapState = (state: RootState) => {
+  const { tMinus, targetTime, currentTime } = state.timerSlice;
+  return { tMinus, targetTime, currentTime };
+};
+
+const mapDispatch = { advanceTimeBy, flipCounting };
+
+export default connect(mapState, mapDispatch)(Timer);
