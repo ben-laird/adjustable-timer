040e1146bdfbfd92be5bbcb479c1f4163fc0461a
ben-laird <86321957+ben-laird@users.noreply.github.com>
2022-06-29 10:41:10 -0500

    Mod timeStone to use dayjs for counter and store

--- src/features/timer/Timer.tsx
+++ src/features/timer/Timer.tsx
@@ -1,7 +1,8 @@
+import dayjs from "dayjs";
 import { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { RootState } from "../../app/store";
-import { advanceTimeBy } from "./timerSlice";
+import { advanceTimeBy, DayDelta } from "./timerSlice";
 
 type TimerProps = ReturnType<typeof mapState> & typeof mapDispatch;
 
@@ -9,19 +10,27 @@ const Timer = (props: TimerProps) => {
   const dateCode = "DD MMM, YYYY - h:mm:ss";
   const duraCode = "HH:mm:ss";
 
-  const [drStrangeMode, setDrStrangeMode] = useState(true);
+  const [drStrangeMode, setDrStrangeMode] = useState(false);
   const [timerId, setTimerId] = useState<NodeJS.Timeout | undefined>(undefined);
 
-  const timeStone = () => {
+  const timeStone = (delta: DayDelta) => {
     if (drStrangeMode) {
       clearTimeout(timerId);
       return;
     }
-    props.advanceTimeBy({ value: 2, unit: "seconds" });
-    setTimerId(setTimeout(timeStone, 2000));
+    props.advanceTimeBy(delta);
+
+    const { value, unit } = delta;
+    const deltaMilis = dayjs.duration(value, unit).asMilliseconds();
+
+    setTimerId(
+      setTimeout(() => {
+        timeStone(delta);
+      }, deltaMilis)
+    );
   };
 
-  useEffect(timeStone, [drStrangeMode]);
+  useEffect(() => timeStone({ value: 1, unit: "second" }), [drStrangeMode]);
 
   return (
     <div>
