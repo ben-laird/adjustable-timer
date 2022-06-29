e2caf655b31a226011fbacb13709112239c181a6
ben-laird <86321957+ben-laird@users.noreply.github.com>
2022-06-29 13:10:10 -0500

    Implement Start/Stop button component

--- src/features/timer/StartStop.tsx
+++ src/features/timer/StartStop.tsx
@@ -4,13 +4,20 @@ import { flipCounting } from "./timerSlice";
 
 type StartStopProps = ReturnType<typeof mapState> & typeof mapDispatch;
 
-const StartStop = () => {
-  const memes = () => console.log("memes");
-  return <button onClick={memes}>Start/Stop Timer</button>;
+const StartStop = (props: StartStopProps) => {
+  return (
+    <button onClick={props.flipCounting}>
+      {`${props.isCounting ? "Stop" : "Start"}`} Timer
+    </button>
+  );
 };
 
-const mapState = (state: RootState) => {};
+const mapState = (state: RootState) => {
+  const { isCounting } = state.timerSlice;
+  const output = { isCounting };
+  return output;
+};
 
 const mapDispatch = { flipCounting };
 
-export default StartStop;
+export default connect(mapState, mapDispatch)(StartStop);
