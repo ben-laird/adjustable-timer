70a896ca176c344c1e9fb7d0769fbce2f38debbe
ben-laird <86321957+ben-laird@users.noreply.github.com>
2022-06-30 17:38:06 -0500

    Add extra styling to start/stop button

--- src/features/timer/StartStop.tsx
+++ src/features/timer/StartStop.tsx
@@ -1,5 +1,6 @@
-import { Alert, Button, Snackbar } from "@mui/material";
-import React, { useState } from "react";
+import { StartRounded, Stop } from "@mui/icons-material";
+import { Alert, Button, Grid, Snackbar } from "@mui/material";
+import { Fragment, SyntheticEvent, useState } from "react";
 import { connect } from "react-redux";
 import { RootState } from "../../app/store";
 import { flipCounting } from "./timerSlice";
@@ -14,25 +15,30 @@ const StartStop = (props: StartStopProps) => {
     setOpen(true);
   };
 
-  const handleClose = (
-    _event?: React.SyntheticEvent | Event,
-    reason?: string
-  ) => {
+  const handleClose = (_event?: SyntheticEvent | Event, reason?: string) => {
     if (reason === "clickaway") return;
     setOpen(false);
   };
 
   return (
-    <React.Fragment>
-      <Button variant="contained" size="large" onClick={handleClick}>
-        {`${props.isCounting ? "Stop" : "Start"}`} Timer
-      </Button>
+    <Fragment>
+      <Grid container={true} justifyContent="center" margin={2}>
+        <Button
+          variant="contained"
+          size="large"
+          onClick={handleClick}
+          color={`${props.isCounting ? "error" : "success"}`}
+          startIcon={props.isCounting ? <Stop /> : <StartRounded />}
+        >
+          {`${props.isCounting ? "Stop" : "Start"}`} Timer
+        </Button>
+      </Grid>
       <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
         <Alert onClose={handleClose} severity="success" sx={{ width: "100%" }}>
           The timer has {`${props.isCounting ? "started!" : "stopped!"}`}
         </Alert>
       </Snackbar>
-    </React.Fragment>
+    </Fragment>
   );
 };
 
