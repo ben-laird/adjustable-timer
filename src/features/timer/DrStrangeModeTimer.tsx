e76bb8a191ac76eaa1b1e57fac9fee399df2aba7
ben-laird <86321957+ben-laird@users.noreply.github.com>
2022-07-01 13:25:07 -0500

    Box children and button

--- src/features/timer/DrStrangeModeTimer.tsx
+++ src/features/timer/DrStrangeModeTimer.tsx
@@ -1,20 +1,29 @@
-import { Button } from "@mui/material";
-import { FC, useState } from "react";
+import { Box, Button } from "@mui/material";
+import { FC, ReactNode, useState } from "react";
 import Timer from "./Timer";
 
-type DrStrangeModeTimerProps = { dateCode: string; durationCode: string };
+type DrStrangeModeTimerProps = {
+  dateCode: string;
+  durationCode: string;
+  children?: ReactNode;
+};
 
 const DrStrangeModeTimer: FC<DrStrangeModeTimerProps> = (props) => {
   const [drStrangeMode, setDrStrangeMode] = useState(true);
   return (
     <Timer {...props} drStrangeMode={drStrangeMode}>
-      <Button
-        variant="text"
-        size="small"
-        onClick={() => setDrStrangeMode(!drStrangeMode)}
-      >
-        Turn {`${drStrangeMode ? "off" : "on"}`} Dr. Strange Mode
-      </Button>
+      <Box display="flex" justifyContent="flex-end" alignItems="flex-end">
+        <Button
+          variant="text"
+          size="small"
+          color={`${drStrangeMode ? "success" : "primary"}`}
+          sx={{margin: 2, padding: 0}}
+          onClick={() => setDrStrangeMode(!drStrangeMode)}
+        >
+          Turn {`${drStrangeMode ? "off" : "on"}`} Dr. Strange Mode
+        </Button>
+        {props.children}
+      </Box>
     </Timer>
   );
 };
