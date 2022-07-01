599e37c42675a180593dd097d00150d09575b18b
ben-laird <86321957+ben-laird@users.noreply.github.com>
2022-07-01 12:28:29 -0500

    Clean up StartStop

--- src/features/timer/StartStop.tsx
+++ src/features/timer/StartStop.tsx
@@ -1,13 +1,11 @@
 import { StartRounded, Stop } from "@mui/icons-material";
 import { Alert, Button, Grid, Snackbar } from "@mui/material";
-import { Fragment, SyntheticEvent, useState } from "react";
-import { connect } from "react-redux";
+import { FC, Fragment, SyntheticEvent, useState } from "react";
+import { connect, ConnectedProps } from "react-redux";
 import { RootState } from "../../app/store";
 import { flipCounting } from "./timerSlice";
 
-type StartStopProps = ReturnType<typeof mapState> & typeof mapDispatch;
-
-const StartStop = (props: StartStopProps) => {
+const StartStop: FC<ConnectedProps<typeof connector>> = (props) => {
   const [open, setOpen] = useState(false);
 
   const handleClick = () => {
@@ -42,10 +40,11 @@ const StartStop = (props: StartStopProps) => {
   );
 };
 
-const mapState = (state: RootState) => {
-  return { isCounting: state.timerSlice.isCounting };
-};
+const connector = connect(
+  (state: RootState) => {
+    return { isCounting: state.timerSlice.isCounting };
+  },
+  { flipCounting }
+);
 
-const mapDispatch = { flipCounting };
-
-export default connect(mapState, mapDispatch)(StartStop);
+export default connector(StartStop);
